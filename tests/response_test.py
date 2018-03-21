b29ef436d5083583e775b3784e628a31b61addb5
wulingqi <wlqfzs507@gmail.com>
2018-03-21 22:15:26 +0800

    add send email function

--- tests/response_test.py
+++ tests/response_test.py
@@ -2,27 +2,16 @@ import unittest
 from response import *
 
 class TestResponse(unittest.TestCase):
-
     def test_basic_response_01(self):
-        resp = basic_response(SUCCESS)
-        self.assertEqual(resp["code"], SUCCESS)
-        self.assertEqual(resp["msg"], defaultCodeMsg[SUCCESS])
-        self.assertEqual(resp["data"], None)
+        resp = basic_response(1)
+        self.assertEqual(resp['msg'], True)
 
     def test_basic_response_02(self):
-        resp = basic_response(ERROR)
-        self.assertEqual(resp["code"], ERROR)
-        self.assertEqual(resp["msg"], defaultCodeMsg[ERROR])
-        self.assertEqual(resp["data"], None)
+        resp = basic_response(2)
+        self.assertEqual(resp['msg'], True)
 
     def test_basic_response_03(self):
-        resp = basic_response(-1)
-        self.assertEqual(resp["code"], -1)
-        self.assertEqual(resp["msg"], None)
-        self.assertEqual(resp["data"], None)
-
-    def test_basic_response_04(self):
-        self.assertRaises(ValueError, basic_response, '1')
+        self.assertRaises(ValueError, basic_response, 'z')
 
 if __name__ == '__main__':
     unittest.main()
