f68f74e9e20d8604ebd1eae8623275c87d17e734
Vimsucks <dev@vimsucks.com>
2018-03-21 23:20:51 +0800

    fix unittest issue

--- tests/response_test.py
+++ tests/response_test.py
@@ -4,16 +4,31 @@ from response import *
 
 
 class TestResponse(unittest.TestCase):
+
+    # test with SUCCESS response code
     def test_basic_response_01(self):
-        resp = basic_response(1)
-        self.assertEqual(resp['msg'], True)
+        resp = basic_response(SUCCESS)
+        self.assertEqual(resp["code"], SUCCESS)
+        self.assertEqual(resp["msg"], defaultCodeMsg[SUCCESS])
+        self.assertEqual(resp["data"], None)
 
+    # test with ERROR response code
     def test_basic_response_02(self):
-        resp = basic_response(2)
-        self.assertEqual(resp['msg'], True)
+        resp = basic_response(ERROR)
+        self.assertEqual(resp["code"], ERROR)
+        self.assertEqual(resp["msg"], defaultCodeMsg[ERROR])
+        self.assertEqual(resp["data"], None)
 
+    # test with undefined response code
     def test_basic_response_03(self):
-        self.assertRaises(ValueError, basic_response, 'z')
+        resp = basic_response(-1)
+        self.assertEqual(resp["code"], -1)
+        self.assertEqual(resp["msg"], None)
+        self.assertEqual(resp["data"], None)
+
+    # test with incompatible value type
+    def test_basic_response_04(self):
+        self.assertRaises(ValueError, basic_response, '-1')
 
 
 if __name__ == '__main__':
